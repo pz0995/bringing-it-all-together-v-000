44e5e91ac063989c399b31b484be6508912f79fd
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-09-22 02:58:13 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+
+  attr_accessor :id, :name, :breed
+
+  def initialize(id=nil, name:, breed:)
+    id = @id
+    name = @name
+    breed = @breed
+  end
+end
+
+class Dog_Table
+  def create_table
+    DB[:conn].execute("DROP TABLE IF EXISTS dogs")
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+        )
+    SQL
+  end
+end
